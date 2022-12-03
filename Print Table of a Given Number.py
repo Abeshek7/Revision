a5d4a813976ce1d0a6837130f3718bc348651eb1
Abeshek7 <abeshek14live7@gmail.com>
2022-12-03 17:37:25 +0530

    Commented sum of nos from 1 to 100

--- Print Table of a Given Number.py
+++ Print Table of a Given Number.py
@@ -5,6 +5,7 @@ for i in range(1,11):
     print(number,'x',i,"=", number*i)
 
 #16) How to print sum of numbers starting from 1 to 100?
-#print sum(range(1,101))
+# var1=sum(range(1,101))
+# print(var1)
 
 
