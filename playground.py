0d1d6af78b5a4a7ece3ee2b857b937721163752b
Abeshek7 <abeshek14live7@gmail.com>
2022-12-03 14:14:02 +0530

    Added comma here

--- playground.py
+++ playground.py
@@ -1,20 +1,22 @@
-
 # 1.sorting in ascending Order
 # mylist = [5,8,9,6,3]
 # new = sorted(mylist)
 # print(new)
-
+# ------------------------------------------------------------
 # 2.sorting in descending Order
 # list =[10,20,30,40,50]
 # desc =sorted(list,reverse=True)
 # print(desc)
+# ------------------------------------------------------------
+# 3.Check string is plaindrome or not.
 
-#3.Check string is plaindrome or not.
+# var1 =input("Enter the String :")
+# sto =var1[::-1]
+#
+# if(var1 ==sto):
+#     print("The String is a Palindrome")
+# else:
+#     print("The String is not a palindrome")
+# ------------------------------------------------------------
 
-var1 =input("Enter the String :")
-sto =var1[::-1]
 
-if(var1 ==sto):
-    print("The String is a Palindrome")
-else:
-    print("The String is not a palindrome")
