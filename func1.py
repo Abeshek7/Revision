715e127ecb9b67e199c96dfd0c55a8529de2f2cf
Abeshek7 <abeshek14live7@gmail.com>
2022-12-03 16:53:00 +0530

    Using functions in one python file and import these functions and using them in a separate file

---
+++ func1.py
@@ -0,0 +1,6 @@
+
+def calculate_area_square(a):
+    return a*a
+
+def calculate_area_triangle(b,h):
+    return 1/2*(b*h)
---
+++ maths.py
@@ -0,0 +1,8 @@
+
+import func1 as f
+
+area =f.calculate_area_square(5)
+print(area)
+
+triangle=f.calculate_area_triangle(10,4)
+print(triangle)
